5552e88411b10303b523a660c0ff24ba198728bd
BryanDevelops98 <63466793+BryanDevelops98@users.noreply.github.com>
2021-11-15 15:33:08 -0500

    Add files via upload

---
+++ djangoExample/webExample/static/js/javascript.js
@@ -0,0 +1,15 @@
+let myimg = new Array(
+    "/static/img/city.jpg",
+    "/static/img/japan.jpg",
+    "/static/img/bird.jpg",
+    "/static/img/ecuador.jpg");
+
+    function slidePic (){
+        if (){
+
+        }
+        else{
+            
+        }
+
+    }
